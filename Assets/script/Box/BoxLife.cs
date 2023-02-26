b8c61c6c014488205565e5d42aa71ed423cc0623
FRICKE-PC30\FRICKE-PC30 <nuttapon.a6@mail.kmutt.ac.th>
2023-02-26 21:17:15 +0700

    Add Particle , Despawn

--- Assets/script/Box/BoxLife.cs
+++ Assets/script/Box/BoxLife.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class BoxLife : MonoBehaviour
+public class BoxLife : NetworkBehaviour
 {
     public int hp = 3;
+    public GameObject BoxParticle;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bullet")
         {
             hp--;
+            collision.gameObject.GetComponent<NetworkObject>().Despawn();
+            SpawnBombEffect();
             Destroy(collision.gameObject);
         }
         if (hp <= 0)
         {
+            gameObject.GetComponent<NetworkObject>().Despawn();
             Destroy(gameObject);
         }
+    }
+    private void SpawnBombEffect()
+    {
+        GameObject BoxParticleEffect = Instantiate(BoxParticle, transform.position, Quaternion.identity);
+        BoxParticleEffect.GetComponent<NetworkObject>().Spawn();
     }
 }
