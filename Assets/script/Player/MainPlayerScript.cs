90bc5f6630ce3b4ebab2b1b508bf97fe505912c9
FRICKE-PC30\FRICKE-PC30 <nuttapon.a6@mail.kmutt.ac.th>
2023-03-08 01:18:30 +0700

    Add new tank model + Animation

--- Assets/script/Player/MainPlayerScript.cs
+++ Assets/script/Player/MainPlayerScript.cs
@@ -8,7 +8,7 @@ using Unity.Collections;
 public class MainPlayerScript : NetworkBehaviour
 {
     public float speed = 5.0f;
-    public float rotationSpeed = 3.0f;
+    public float rotationSpeed = 1.0f;
     Rigidbody rb;
 
     public TMP_Text namePrefab;
@@ -133,15 +133,15 @@ public class MainPlayerScript : NetworkBehaviour
     }
     private void FixedUpdate()
     {
-        if (IsOwner)
-        {
-            float translation = Input.GetAxis("Vertical") * speed;
-            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-            translation *= Time.deltaTime;
-            Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
-            rb.MovePosition(rb.position + this.transform.forward * translation);
-            rb.MoveRotation(rb.rotation * turn);
-        }
+        //if (IsOwner)
+        //{
+        //    float translation = Input.GetAxis("Vertical") * speed;
+        //    float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //    translation *= Time.deltaTime;
+        //    Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+        //    rb.MovePosition(rb.position + this.transform.forward * translation);
+        //    rb.MoveRotation(rb.rotation * turn);
+        //}
     }
 
     //For Name Tag --------------------------------
---
+++ Assets/script/Player/PlayerControllerScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControllerScript : NetworkBehaviour
+{
+    public float speed = 3.0f;
+    public float rotationSpeed = 1.0f;
+
+    private Animator animator;
+    private Rigidbody rb;
+    private bool running;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        running = false;
+    }
+
+    void moveForward()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Mathf.Abs(verticalInput) > 0.01f)
+        {
+            // move forward only
+            if (verticalInput > 0.01f)
+            {
+                float translation = verticalInput * speed;
+                translation *= Time.fixedDeltaTime;
+                rb.MovePosition(rb.position + this.transform.forward * translation);
+
+                if (!running)
+                {
+                    running = true;
+                    animator.SetBool("Walking", true);
+                }
+            }
+        }
+        else if (running)
+        {
+            running = false;
+            animator.SetBool("Walking", false);
+        }
+    }
+
+    void turn()
+    {
+        float rotation = Input.GetAxis("Horizontal");
+        if (rotation != 0)
+        {
+            rotation *= rotationSpeed;
+            Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+            rb.MoveRotation(rb.rotation * turn);
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsOwner) return;
+        moveForward();
+        turn();
+    }
+}
